6d8e9534f21fc425b836a260edfe725317f9b931
bhawanthas <bpsshanuka@gmail.com>
2022-01-11 22:21:38 +0530

    hot fix for search page in tillit

--- src/app/features/search-page/search-page.component.ts
+++ src/app/features/search-page/search-page.component.ts
@@ -15,33 +15,24 @@ import { encode, decode } from 'js-base64';
   styleUrls: ['./search-page.component.css']
 })
 export class SearchPageComponent implements OnInit {
-
   searchId: string;
   results: any = [];
   errorOccurred: boolean = false;
   otherResultsAvailable: boolean = false;
-
   tdpObsCount: number = 0;
   tdpObsResCount: number = 0;
   tdpObsResolved: boolean = true;
   tdpErrorCount: number = 0;
-
   error: ErrorMessage;
   loadingComplete: boolean = false;
-
-  // Loader Variables
-
+  // Loader Variable
   color = "primary";
   mode = "indeterminate";
   value = 20;
-
   page:number = 1;
   perPage:number = 10;
   NoItems:number;
-  
-
   constructor(private route: ActivatedRoute, private transactionDataService: TransactionDataService) { }
-
   ngOnInit() {
     this.route.params.subscribe((data) => {
       this.results = [];
@@ -53,7 +44,6 @@ export class SearchPageComponent implements OnInit {
       }
     });
   }
-  
   onChangePage(event:number){
     this.route.params.subscribe((data) => {
      this.page = event;
@@ -66,15 +56,14 @@ export class SearchPageComponent implements OnInit {
     }
   });
   }
-
   reloadCurrentPage() {
     window.location.reload();
    }
-
   search(id: string): void {
     this.transactionDataService.getTransactions(id,this.page,this.perPage).subscribe((transactions) => {
    //   console.log("Transactions: ", transactions);
       this.errorOccurred = false;
+      if(!!transactions){
       transactions.forEach(element => {
       // console.log("Blockchain: ", element);
         this.NoItems = element.Itemcount 
@@ -158,8 +147,8 @@ export class SearchPageComponent implements OnInit {
             txnHash: element.Txnhash,
             txnUrl: element.Url,
             identifier: element.Identifier,
-            sender: element.From,
-            receiver: element.To,
+            sender: "",
+            receiver: "",
             timestamp: element.Timestamp,
             transferType: element.TxnType,
             sequence: element.SequenceNo,
@@ -239,8 +228,16 @@ export class SearchPageComponent implements OnInit {
           this.otherResultsAvailable = true;
        //   console.log("ELSE: ", txnItem);
         }
-      });
-
+      })}else{
+        this.loadingComplete = true;
+        this.errorOccurred = true;
+        this.error = {
+          errorTitle: "No matching results found",
+          errorMessage: "We can not find the requested records in Stellar blockchain",
+          errorMessageSecondary: "Check if the entered ID is correct and try again.",
+          errorType: "empty"
+        }
+      }
       if (this.otherResultsAvailable) {
         this.loadingComplete = true;
       } else if (!this.otherResultsAvailable && this.error && this.tdpErrorCount == this.tdpObsCount) {
@@ -251,28 +248,34 @@ export class SearchPageComponent implements OnInit {
       } else {
         this.loadingComplete = true;
       }
-
     }, (err) => {
-     // console.log("Blockchain Error: ", err);
+
+    console.log("err.error.code",err.error.code)
       this.loadingComplete = true;
       this.errorOccurred = true;
-      if (err.status === 400) {
+      if (err.error.code === 400) {
         this.error = {
           errorTitle: "No matching results found",
-          errorMessage: "There is no data associated with the given ID. Check if the entered ID is correct and try again.",
-          errorMessageSecondary: "If you still don't see the results you were expecting, please let us know.",
+          errorMessage:  "We can not find the requested records in Stellar blockchain",
+          errorMessageSecondary: "Please try again later",
+          errorType: "empty"
+        }
+      }
+      else if(err.error.code === 500){
+        this.error = {
+          errorTitle: "Internal server error",
+          errorMessage:  "We can not find the requested records in Stellar blockchain",
+          errorMessageSecondary: "Please try again later",
           errorType: "empty"
         }
       } else {
         this.error = {
-          errorTitle: "Something went wrong",
-          errorMessage: "An error occurred while retrieving data. Check if the entered ID is correct and try again in a while.",
-          errorMessageSecondary: "If you still don't see the results you were expecting, please let us know.",
+          errorTitle: "Somthing Went Wrong",
+          errorMessage: "Unable to reach Tillit explorer gateway",
+          errorMessageSecondary: "Please try again later",
           errorType: "empty"
         }
       }
     });
   }
-
-
 }
